f88fe1519bef1b2f17345a3937805072f285a182
allamale <alanale03@gmail.com>
2026-05-18 11:27:42 +0200

    added some front-end to the planroutes and and trackinvoices. Also added the navigation to each page from the burger menu.

---
+++ frontend/src/pages/TrackOrders.tsx
@@ -0,0 +1,12 @@
+import { Container } from "react-bootstrap";
+
+function TrackOrders() {
+  return (
+    <Container className="p-4">
+      <h1>Track Orders</h1>
+      <p>This is the Track Orders placeholder page.</p>
+    </Container>
+  );
+}
+
+export default TrackOrders;
