b9c10cd7ff88b1586b680b287017f3534a2d1b53
Matteo <matteo.heidelberger@viacesi.fr>
2026-05-21 09:54:22 +0200

    feat(notification): added the first redis EventBus, we can create notifications via events now

--- backend/services/notification/app/config.py
+++ backend/services/notification/app/config.py
@@ -6,6 +6,7 @@ class Settings(BaseSettings):
     jwt_secret: str
     jwt_algorithm: str = "HS256"
     access_token_expire_minutes: int = 60
+    redis_url: str = "redis://redis:6379"
 
 
 settings = Settings()
